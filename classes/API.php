d72dacfcb123143b7962fb147cdf88ac34e222c2
Boone B Gorges <boonebgorges@gmail.com>
2023-06-19 21:24:54 -0500

    Exclude pages from 'Add Existing Page' search when it's associated with a module.

--- classes/API.php
+++ classes/API.php
@@ -46,6 +46,8 @@ class API {
 	 */
 	public function init() {
 		add_action( 'rest_api_init', [ $this, 'register_endpoints' ] );
+
+		add_filter( 'rest_page_query', [ $this, 'filter_page_query' ], 10, 2 );
 	}
 
 	/**
@@ -61,5 +63,32 @@ class API {
 			$this->endpoints[ $class_name ] = new $class_name_with_namespace();
 			$this->endpoints[ $class_name ]->register_routes();
 		}
+	}
+
+	/**
+	 * Filters 'page' REST queries for the excludeModulePages parameter.
+	 *
+	 * @param mixed[]          $args    Query arguments.
+	 * @param \WP_REST_Request $request Request object.
+	 * @return mixed[]
+	 */
+	public function filter_page_query( $args, $request ) {
+		$exclude_module_pages = $request->get_param( 'excludeModulePages' );
+
+		if ( ! $exclude_module_pages ) {
+			return $args;
+		}
+
+		$tax_query = isset( $args['tax_query'] ) && is_array( $args['tax_query'] ) ? $args['tax_query'] : [];
+
+		$tax_query[] = [
+			'taxonomy' => Schema::get_module_taxonomy(),
+			'operator' => 'NOT EXISTS',
+		];
+
+		// phpcs:ignore WordPress.DB.SlowDBQuery
+		$args['tax_query'] = $tax_query;
+
+		return $args;
 	}
 }
