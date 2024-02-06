2b2059e26cf2b45f7457613d260d8f104713f424
Boone B Gorges <boonebgorges@gmail.com>
2024-02-06 16:17:27 -0600

    Asset build.

---
+++ build/webwork-problem-embed.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'bdadbcf182a246c9372a');
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'openlab-at-city-tech/openlab-modules',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b43421e5e25e8894d8a4d2ae593e260bc796cedb',
+        'reference' => 'a43d1232f9a1a25bd5cca645a33cda7a407d7489',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'openlab-at-city-tech/openlab-modules' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b43421e5e25e8894d8a4d2ae593e260bc796cedb',
+            'reference' => 'a43d1232f9a1a25bd5cca645a33cda7a407d7489',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
