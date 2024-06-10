7ab8b3a5d61f6f3ddc0963858f0b462c9133d5dc
Boone B Gorges <boonebgorges@gmail.com>
2024-06-10 14:26:01 -0500

    Ensure that 'sharing notice' for Sharing block is reactive with the Share toggle.

--- assets/src/blocks/sharing/edit.js
+++ assets/src/blocks/sharing/edit.js
@@ -33,10 +33,22 @@ export default function Edit( { attributes } ) {
 
 		const thisPageModuleId = thisPageModuleIdCb()
 
-		const thisModule = select( 'core' ).getEntityRecord( 'postType', 'openlab_module', thisPageModuleId )
+		/*
+		 * If the page currently being edited is thisPageModuleId, then we should get the
+		 * value of enableSharing directly from getEditedPostAttribute() so that it's
+		 * properly reactive.
+		 */
+		const getEnableSharing = () => {
+			if ( thisPageModuleId === thisPostId ) {
+				return select( 'core/editor' ).getEditedPostAttribute( 'enableSharing' )
+			}
+
+			const thisModule = select( 'core' ).getEntityRecord( 'postType', 'openlab_module', thisPageModuleId )
+			return thisModule?.enableSharing || false
+		}
 
 		return {
-			isSharingEnabled: thisModule?.enableSharing || false,
+			isSharingEnabled: getEnableSharing(),
 		};
 	} );
 
@@ -57,9 +69,11 @@ export default function Edit( { attributes } ) {
 				className="clone-module-button clone-module-button-reset"
 			>{ __( 'Clone this Module', 'openlab-modules' ) }</button>
 
-			<p className="sharing-notice">
-				{ __( 'Sharing is disabled for this module, so the "Clone this Module" button will not show on the front end. Remove this block, or enable sharing under the "Edit Module" settings in the Module toolbar.', 'openlab-modules' ) }
-			</p>
+			{ ! isSharingEnabled && (
+				<p className="sharing-notice">
+					{ __( 'Sharing is disabled for this module, so the "Clone this Module" button will not show on the front end. Remove this block, or enable sharing under the "Edit Module" settings in the Module toolbar.', 'openlab-modules' ) }
+				</p>
+			) }
 		</div>
 	)
 }
