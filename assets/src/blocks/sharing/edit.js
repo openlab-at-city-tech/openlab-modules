c43d4d88b5d36d20a15fafc5bc6018efd1225047
Boone B Gorges <boonebgorges@gmail.com>
2024-06-10 11:59:04 -0500

    Refinements to behavior of 'Clone this Module' button when sharing is disabled.

--- assets/src/blocks/sharing/edit.js
+++ assets/src/blocks/sharing/edit.js
@@ -1,7 +1,8 @@
 import { useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
 
-import './block.scss';
+import './editor.scss';
 
 /**
  * Edit function.
@@ -12,11 +13,53 @@ import './block.scss';
 export default function Edit( { attributes } ) {
 	const {} = attributes
 
+	const { isSharingEnabled } = useSelect( ( select ) => {
+		const thisPostId = select( 'core/editor' ).getCurrentPostId()
+		const postType = select( 'core/editor' ).getCurrentPostType()
+
+		const thisPageModuleIdCb = () => {
+			if ( 'openlab_module' === postType ) {
+				return thisPostId
+			}
+
+			/*
+			if ( thisPostModuleIds ) {
+				return thisPostModuleIds[ 0 ]
+			}
+			*/
+
+			return 0
+		}
+
+		const thisPageModuleId = thisPageModuleIdCb()
+
+		const thisModule = select( 'core' ).getEntityRecord( 'postType', 'openlab_module', thisPageModuleId )
+
+		return {
+			isSharingEnabled: thisModule?.enableSharing || false,
+		};
+	} );
+
+	const useCustomBlockProps = () => {
+		let className = 'wp-block-openlab-modules-sharing'
+		if ( ! isSharingEnabled ) {
+			className += ' sharing-is-disabled'
+		}
+
+		return useBlockProps( {
+			className
+		} );
+	}
+
 	return (
-		<div { ...useBlockProps() }>
+		<div { ...useCustomBlockProps() }>
 			<button
 				className="clone-module-button clone-module-button-reset"
 			>{ __( 'Clone this Module', 'openlab-modules' ) }</button>
+
+			<p className="sharing-notice">
+				{ __( 'Sharing is disabled for this module, and the "Clone this Module" button will not show on the front end. Remove this block, or enable sharing under the "Edit Module" settings in the Module toolbar.', 'openlab-modules' ) }
+			</p>
 		</div>
 	)
 }
