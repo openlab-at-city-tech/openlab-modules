215dfc0c14b4a2f67bed54033a32ae52b4370f66
Boone B Gorges <boonebgorges@gmail.com>
2024-07-10 12:03:24 -0500

    Asset build.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-plugins', 'wp-server-side-render', 'wp-url'), 'version' => '871b09f0990514e88462');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-plugins', 'wp-server-side-render', 'wp-url'), 'version' => 'd258b0e21fd5e3631074');
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'openlab-at-city-tech/openlab-modules',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '3a6a0c58756eee38949760c22b766efc49663307',
+        'reference' => '1e90db48fb1613a64bb41ca459d2ad69419b0674',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'openlab-at-city-tech/openlab-modules' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '3a6a0c58756eee38949760c22b766efc49663307',
+            'reference' => '1e90db48fb1613a64bb41ca459d2ad69419b0674',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
