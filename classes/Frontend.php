openlab-at-city-tech/openlab-modules

commit 22a82d733dd92760cc7180170e44d44513f404e3

First pass at H5P integration.
Requires h5p-postmessage plugin on source site for H5P content.

classes/Frontend.php
@@ -40,6 +40,7 @@ class Frontend {
 	 */
 	public function init() {
 		add_action( 'wp_enqueue_scripts', [ $this, 'register_assets' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets' ], 20 );
 
 		add_filter( 'the_content', [ __CLASS__, 'append_pagination' ] );
 
@@ -62,12 +63,57 @@ class Frontend {
 			true
 		);
 
+		$current_page_permalink = get_permalink();
+
+		wp_localize_script(
+			'openlab-modules-frontend',
+			'openlabModulesStrings',
+			[
+				'continueWithout'   => __( 'Continue without logging in', 'openlab-modules' ),
+				'dismiss'           => __( 'Dismiss', 'openlab-modules' ),
+				'logIn'             => __( 'Log In', 'openlab-modules' ),
+				'sectionComplete'   => __( 'You have completed this page. You will receive a private message confirming the completion.', 'openlab-modules' ),
+				'toReceiveCredit'   => __( 'To receive an official confirmation when you complete this page, please sign in now.', 'openlab-modules' ),
+				'youAreNotLoggedIn' => __( 'You are not logged in.', 'openlab-modules' ),
+			]
+		);
+
+		wp_add_inline_script(
+			'openlab-modules-frontend',
+			'const openlabModules = ' . wp_json_encode(
+				[
+					'ajaxUrl'        => admin_url( 'admin-ajax.php' ),
+					'isUserLoggedIn' => is_user_logged_in(),
+					'loginUrl'       => wp_login_url( (string) $current_page_permalink ),
+					'nonce'          => wp_create_nonce( 'openlab-modules' ),
+					'postId'         => get_queried_object_id(),
+				]
+			),
+			'before'
+		);
+
 		wp_register_style(
 			'openlab-modules-frontend',
 			OPENLAB_MODULES_PLUGIN_URL . '/build/frontend.css',
 			[],
 			$blocks_asset_file['version']
 		);
+	}
+
+	/**
+	 * Enqueues assets.
+	 *
+	 * For the time being, our frontend is loaded on every module or module page,
+	 * since it's hard to detect whether they're needed based on the presence
+	 * of integration embeds.
+	 *
+	 * @return void
+	 */
+	public function enqueue_assets() {
+		if ( self::get_module_id_for_post() ) {
+			wp_enqueue_script( 'openlab-modules-frontend' );
+			wp_enqueue_style( 'openlab-modules-frontend' );
+		}
 	}
 
 	/**
@@ -88,17 +134,7 @@ class Frontend {
 			return $content;
 		}
 
-		$module_id = null;
-		if ( Schema::get_module_post_type() === get_post_type() ) {
-			$is_module = true;
-			$module_id = get_queried_object_id();
-		} else {
-			$is_module  = false;
-			$module_ids = Module::get_module_ids_of_page( get_queried_object_id() );
-			if ( $module_ids ) {
-				$module_id = $module_ids[0];
-			}
-		}
+		$module_id = self::get_module_id_for_post();
 
 		if ( ! $module_id ) {
 			return $content;
@@ -108,6 +144,8 @@ class Frontend {
 		if ( ! $module ) {
 			return $content;
 		}
+
+		$is_module = Schema::get_module_post_type() === get_post_type( $module_id );
 
 		// Offset keys so that the module page is 0.
 		$module_page_ids = $module->get_page_ids();
@@ -219,21 +257,13 @@ class Frontend {
 			return;
 		}
 
-		$module_id = null;
-		if ( Schema::get_module_post_type() === get_post_type( $post ) ) {
-			$is_module = true;
-			$module_id = $post_id;
-		} else {
-			$is_module  = false;
-			$module_ids = Module::get_module_ids_of_page( $post_id );
-			if ( $module_ids ) {
-				$module_id = $module_ids[0];
-			}
-		}
+		$module_id = self::get_module_id_for_post( $post_id );
 
 		if ( ! $module_id ) {
 			return;
 		}
+
+		$is_module = Schema::get_module_post_type() === get_post_type( $post );
 
 		if ( function_exists( 'messages_new_message' ) ) {
 			// translators: 1. Module title, 2. Module URL.
@@ -265,5 +295,41 @@ class Frontend {
 				]
 			);
 		}
+	}
+
+	/**
+	 * Get module ID for a post ID.
+	 *
+	 * For modules, this returns the post ID itself. For module pages,
+	 * this returns the module ID.
+	 *
+	 * @param int $post_id Optional. Post ID. Default is the current post.
+	 * @return int|null
+	 */
+	public static function get_module_id_for_post( $post_id = 0 ) {
+		if ( ! $post_id ) {
+			$post_id = get_the_ID();
+		}
+
+		if ( ! $post_id ) {
+			return null;
+		}
+
+		$post = get_post( $post_id );
+
+		if ( ! $post ) {
+			return null;
+		}
+
+		if ( Schema::get_module_post_type() === get_post_type( $post ) ) {
+			return $post_id;
+		}
+
+		$module_ids = Module::get_module_ids_of_page( $post_id );
+		if ( $module_ids ) {
+			return $module_ids[0];
+		}
+
+		return null;
 	}
 }

assets/src/frontend.js
@@ -1,4 +1,5 @@
 import './shared.scss'
+import './frontend.scss'
 
 import './blocks/module-attribution/block.scss'
 import './blocks/module-list/block.scss'
@@ -6,3 +7,5 @@ import './blocks/module-navigation/block.scss'
 import './blocks/sharing/block.scss'
 
 import './blocks/sharing/sharing-frontend.js'
+
+import './integrations/completion-bus.js'

assets/src/frontend.scss
@@ -1,0 +1,37 @@
+.module-section-complete-notice-overlay {
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background-color: rgba(255,255,255,0.9);
+	text-align: center;
+	transition: opacity 0.5s ease-in-out;
+}
+
+.module-section-complete-notice {
+	padding: 20px 50px;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	background-color: #fff;
+	border-radius: 5px;
+	border: 1px solid #333;
+
+	p {
+		margin: 0;
+		padding: 0;
+	}
+}
+
+.module-section-complete-notice-dismiss {
+	position: absolute;
+	top: 0;
+	right: 0;
+	padding: 10px;
+	cursor: pointer;
+	color: #666;
+	background-color: #fff;
+	border-radius: 5px;
+}

assets/src/integrations/completion-bus.js
@@ -1,0 +1,101 @@
+/* global openlabModules, openlabModulesStrings */
+
+(() => {
+	document.addEventListener( 'DOMContentLoaded', () => {
+		window.moduleProblemCompletionBus = moduleProblemCompletionBus;
+	})
+})()
+
+const moduleProblemCompletionBus = {
+	completeRequestSent: false,
+	problems: [],
+
+	addProblem( problemId ) {
+		if ( ! this.problems.find( problem => problem.id === problemId ) ) {
+			this.problems.push( { id: problemId, complete: false } );
+		}
+	},
+
+	setProblemComplete( problemId ) {
+		const theProblem = this.problems.find( problem => problem.id === problemId );
+		if ( theProblem ) {
+			theProblem.complete = true;
+			this.checkAllProblemsComplete();
+		}
+	},
+
+  checkAllProblemsComplete() {
+		const allProblemsComplete = this.problems.every( problem => problem.complete );
+
+		if ( allProblemsComplete ) {
+			this.markSectionComplete();
+		}
+	},
+
+	markSectionComplete() {
+		if ( this.completeRequestSent ) {
+			return;
+		}
+
+		const { nonce, postId } = openlabModules;
+
+		this.sendCompleteStatus( nonce, postId ).then( () => {
+			this.completeRequestSent = true;
+			console.log( 'Successfully marked section as complete.' );
+		})
+
+		this.createSectionCompleteNotice();
+	},
+
+	/**
+	 * Async callback for sending 'complete' status to the server.
+	 *
+	 * @param {string} nonce  The nonce for the request.
+	 * @param {number} postId The post ID for the request.
+	 * @return {Promise} The fetch promise.
+	 */
+	async sendCompleteStatus( nonce, postId ) {
+		const { ajaxUrl } = openlabModules;
+		const endpointUrl = `${ajaxUrl}?action=mark_module_section_complete`;
+
+		const body = new URLSearchParams();
+		body.append( 'nonce', nonce );
+		body.append( 'postId', postId );
+
+		const response = await fetch( endpointUrl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: body.toString(),
+		} );
+
+		return response.json();
+	},
+
+	/**
+	 * Creates a notice to display when the section is complete.
+	 *
+	 * @return {void}
+	 */
+	createSectionCompleteNotice() {
+		const notice = document.createElement( 'div' );
+
+		const dismissButton = document.createElement( 'button' );
+		dismissButton.classList.add( 'module-section-complete-notice-dismiss' );
+		dismissButton.innerHTML = '&times; <span class="screen-reader-text">' + openlabModulesStrings.dismiss + '</span>';
+		dismissButton.addEventListener( 'click', () => {
+			overlay.remove();
+		} )
+
+		notice.classList.add( 'module-section-complete-notice' );
+		notice.innerHTML = '<p>' + openlabModulesStrings.sectionComplete + '</p>';
+		notice.appendChild( dismissButton );
+
+		const overlay = document.createElement( 'div' );
+		overlay.classList.add( 'module-section-complete-notice-overlay' );
+		overlay.appendChild( notice );
+
+		document.body.appendChild( overlay );
+	}
+}

assets/src/integrations/h5p/h5p.js
@@ -1,0 +1,28 @@
+(() => {
+	window.addEventListener( 'message', ( event ) => {
+		const { data } = event;
+
+		if ( ! data ) {
+			return;
+		}
+
+		const { objectId, source, verb } = data;
+
+		if ( 'h5p-postmessage' !== source ) {
+			return;
+		}
+
+		console.log( 'h5p integration', verb, objectId, data );
+
+		switch ( verb ) {
+			case 'attempted' :
+				window.moduleProblemCompletionBus.addProblem( objectId );
+				break;
+
+			case 'completed' :
+				window.moduleProblemCompletionBus.setProblemComplete( objectId );
+				break;
+
+		}
+	} );
+})();

classes/App.php
@@ -36,5 +36,9 @@ class App {
 			$wwpe_integration = Integrations\WWPE::get_instance();
 			$wwpe_integration->init();
 		}
+
+		// We don't do a plugin check because H5P content may be embedded from elsewhere.
+		$h5p_integration = Integrations\H5P::get_instance();
+		$h5p_integration->init();
 	}
 }

classes/Integrations/H5P.php
@@ -1,0 +1,132 @@
+<?php
+/**
+ * Integration for h5p.
+ *
+ * @package openlab-modules
+ */
+
+namespace OpenLab\Modules\Integrations;
+
+use OpenLab\Modules\Editor;
+use OpenLab\Modules\Module;
+use OpenLab\Modules\Schema;
+
+/**
+ * Integration for h5p.
+ */
+class H5P {
+	/**
+	 * Private constructor.
+	 *
+	 * @return void
+	 */
+	private function __construct() {}
+
+	/**
+	 * Gets the singleton instance.
+	 *
+	 * @return \OpenLab\Modules\Integrations\WWPE
+	 */
+	public static function get_instance() {
+		static $instance;
+
+		if ( empty( $instance ) ) {
+			$instance = new self();
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Initializes the integration.
+	 *
+	 * @return void
+	 */
+	public function init() {
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets' ], 100 );
+		add_filter( 'openlab_modules_module_requirements', [ $this, 'add_h5p_requirement' ], 10, 2 );
+	}
+
+	/**
+	 * Sets up the post for use with h5p.
+	 *
+	 * @return void
+	 */
+	public function enqueue_assets() {
+		// Only set up data for modules or posts belonging to modules.
+		if ( ! $this->is_module_page() ) {
+			return;
+		}
+
+		$blocks_asset_file = Editor::get_blocks_asset_file( 'h5p' );
+
+		wp_enqueue_script(
+			'openlab-modules-h5p',
+			OPENLAB_MODULES_PLUGIN_URL . '/build/h5p.js',
+			[],
+			$blocks_asset_file['version'],
+			true
+		);
+	}
+
+	/**
+	 * Detects and adds the WWPE requirement for a module.
+	 *
+	 * @param string[] $plugin_requirements Array of plugin requirements.
+	 * @param int      $module_id           Module ID.
+	 * @return string[]
+	 */
+	public function add_h5p_requirement( $plugin_requirements, $module_id ) {
+		// @todo
+		return $plugin_requirements;
+
+
+		// Look for the 'webwork_problem' shortcode in the content of the module and its pages.
+		$contains_wwpe = false;
+
+		$module = Module::get_instance( $module_id );
+		if ( ! $module ) {
+			return $plugin_requirements;
+		}
+
+		$contains_wwpe = $this->post_contains_wwpe( $module_id );
+
+		if ( ! $contains_wwpe ) {
+			$module_page_ids = $module->get_page_ids( 'all' );
+			foreach ( $module_page_ids as $page_id ) {
+				if ( $this->post_contains_wwpe( $page_id ) ) {
+					$contains_wwpe = true;
+					break;
+				}
+			}
+		}
+
+		if ( ! $contains_wwpe ) {
+			return $plugin_requirements;
+		}
+
+		$plugin_requirements['webwork-problem-embed/webwork-problem-embed.php'] = __( 'WebWoRK Problem Embed', 'openlab-modules' );
+
+		return $plugin_requirements;
+	}
+
+	/**
+	 * Determine whether the current page is a module, or a "section" page belonging to a module.
+	 *
+	 * @return bool
+	 */
+	private function is_module_page() {
+		$is_module_page = false;
+		$post           = get_post();
+		if ( is_singular( Schema::get_module_post_type() ) ) {
+			$is_module_page = true;
+		} elseif ( is_singular() ) {
+			$module_ids     = $post ? Module::get_module_ids_of_page( $post->ID ) : [];
+			$is_module_page = ! empty( $module_ids );
+		}
+
+		return $is_module_page;
+	}
+}
+
+

commit cddd79db57851decce8fa32ebcb19e5ba8a4e9aa

When appending pagination, ensure that correct post ID is checked for 'is module' status.
This was inadvertently broken in 22a82d733dd92760cc7180170e44d44513f404e3.

classes/Frontend.php
@@ -145,7 +145,7 @@ class Frontend {
 			return $content;
 		}
 
-		$is_module = Schema::get_module_post_type() === get_post_type( $module_id );
+		$is_module = Schema::get_module_post_type() === get_post_type( get_queried_object_id() );
 
 		// Offset keys so that the module page is 0.
 		$module_page_ids = $module->get_page_ids();