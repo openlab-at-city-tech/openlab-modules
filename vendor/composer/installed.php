4011e96df67e57e17bc6b233a170797621caf354
Boone B Gorges <boonebgorges@gmail.com>
2024-07-30 13:36:28 -0500

    Asset build.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'openlab-at-city-tech/openlab-modules',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a768bf66c16ad6b50d98df18bbad2c4a74a3fe00',
+        'reference' => '90dd75f196fdbe23ebbaca5fb756755e9ff1b2e7',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'openlab-at-city-tech/openlab-modules' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a768bf66c16ad6b50d98df18bbad2c4a74a3fe00',
+            'reference' => '90dd75f196fdbe23ebbaca5fb756755e9ff1b2e7',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
