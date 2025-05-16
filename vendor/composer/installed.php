e6e2d3c5fb824457bf82d02ea69cf560eb90b7e0
Boone B Gorges <boonebgorges@gmail.com>
2025-05-16 15:02:30 -0500

    Asset build.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'openlab-at-city-tech/openlab-modules',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b0b835ce8de4a783cc53697614a7b216a8a7d69f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'hard-g/cpt-tax' => array(
+            'pretty_version' => 'v1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => 'cd87b64316ea5d563770d488845967760d098fca',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../hard-g/cpt-tax',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'league/html-to-markdown' => array(
+            'pretty_version' => '5.1.1',
+            'version' => '5.1.1.0',
+            'reference' => '0b4066eede55c48f38bcee4fb8f0aa85654390fd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/html-to-markdown',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'openlab-at-city-tech/openlab-modules' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b0b835ce8de4a783cc53697614a7b216a8a7d69f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
